9d6bfa198a817161f164b83e230bf9d241c9659f
James Han <in1004kyu@gmail.com>
2016-09-17 21:06:46 +0900

    First commit

---
+++ core.js
@@ -0,0 +1,21 @@
+class Codelab extends React.Component{
+  render() {
+    let text="Hi I am codelab";
+    let style = {
+      backgroundColor : 'aqua'
+    };
+    return(
+      <div style={style}>{text}</div>
+    );
+  }
+}
+
+class App extends React.Component{
+  render() {
+    return(
+      <Codelab/>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
